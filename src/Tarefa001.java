21e918bd9b222ea48cde05f5269ad8c9f9111c0e
Entra21 <entra21@proway.treina>
2021-05-11 19:32:56 -0300

    novo

---
+++ src/Tarefa001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Tarefa001 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String product1 = "Computer";
+        String product2 = "Office e desk";
+
+         int age = 30;
+
+
+    }
+}
