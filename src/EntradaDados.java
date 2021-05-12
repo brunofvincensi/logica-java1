4f919dbd83e7099dd7fd8c469198b2ea8b2da323
Entra21 <entra21@proway.treina>
2021-05-12 19:41:39 -0300

    Resolvendo o problema no print

--- src/EntradaDados.java
+++ src/EntradaDados.java
@@ -3,8 +3,8 @@ public class EntradaDados {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-// quando é botado apenas next na variavel String nao aparecera palavras depois do espaco.
-        String S = scanner.next();
+// quand            //
+// String S = scanner.next();
 
         char variavelQueRecebeSomenteUmCaracter = scanner.next().charAt(0);
 
@@ -15,9 +15,11 @@ public class EntradaDados {
         String s1,s2,s3;
 
         x = scanner.nextInt();
+        scanner.nextLine();
         s1 = scanner.nextLine();
         s2 = scanner.nextLine();
         s3 = scanner.nextLine();
+
 
         System.out.println("Dados digitados: ");
         System.out.println(x);
