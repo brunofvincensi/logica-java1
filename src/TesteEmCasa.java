c8bfe30740a1966dc5b6dcf953ed696763661281
Entra21 <entra21@proway.treina>
2021-05-12 20:29:40 -0300

    modulos matematicos

--- src/TesteEmCasa.java
+++ src/TesteEmCasa.java
@@ -12,9 +12,26 @@ public class TesteEmCasa {
         System.out.println("Cateto adjacente: ");
         double adjacente = scanner.nextDouble();
 
-        double hipotenusa = Math.sqrt(adjacente * adjacente + oposto * oposto);
+        double hipotenusa = Math.sqrt(Math.pow(oposto, 2) + Math.pow(adjacente, 2));
 
         System.out.printf("O valor da hipotenusa é: " + "%.1f%n", hipotenusa);
+
+
+
+
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+
+        double A = Math.pow(x,y);
+
+        System.out.println(x + "elevado a" + y + "=" + A);
+
+        System.out.printf("%.2f elevado a %.2f = %.2f", x, y, A);
+
+        double w = scanner.nextDouble();
+        double W = Math.abs(w);
+        System.out.println(w);
+
 
 
 
