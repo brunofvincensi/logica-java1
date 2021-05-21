677ca5057dc25243a847bd0c3ca3dd54171e1297
Bruno-ferrariv <brunofvincensi@gmail.com>
2021-05-21 16:27:34 -0300

    atualizacao trigonometria

--- src/Trigonometria.java
+++ src/Trigonometria.java
@@ -6,6 +6,9 @@ public class Trigonometria {
         Scanner scanner = new Scanner(System.in);
         Locale.setDefault(Locale.US);
 
+        System.out.println("Hipotenusa: ");
+        double hipotenusa = scanner.nextDouble();
+
         System.out.println("Angulo: ");
         double angulo = scanner.nextDouble();
 
@@ -15,8 +18,7 @@ public class Trigonometria {
         System.out.println("Cateto Oposto: ");
         double oposto = scanner.nextDouble();
 
-        System.out.println("Hipotenusa: ");
-        double hipotenusa = scanner.nextDouble();
+
 
         double hip = oposto/Math.sin(Math.toRadians( angulo ));
 
@@ -28,37 +30,41 @@ public class Trigonometria {
 
         double diagonal = Math.sqrt(Math.pow(adjacente, 2) + Math.pow(oposto, 2));
 
+        double adj2 = Math.sqrt(Math.pow(hipotenusa, 2) - Math.pow(oposto, 2));
+
+        double ops2 = Math.sqrt(Math.pow(hipotenusa, 2) - Math.pow(adjacente, 2));
+
 
 
 
         if(hipotenusa == 0.0F) {
 
             if (adjacente == 0.0F){
-                System.out.printf("Hypotenuse: %.2f%nAdjacente: %.2f%n", hip, adj);
+                System.out.printf("Hipotenusa: %.2f%nCateto Adjacente: %.2f%n", hip, adj);
 
 
             }
 
             if (oposto == 0.0F){
-                System.out.printf("Hypotenuse: %.2f%nOposto: %.2f%n", hip2, ops);
+                System.out.printf("Hipotenusa: %.2f%nCateto Oposto: %.2f%n", hip2, ops);
 
 
             }
 
-
-
+            System.out.printf("Hipotenusa: %.2f%n", diagonal);
 
 
            }
         else
+            if(adjacente == 0.0F)
 
-            System.out.println("");
-
-
-
+                System.out.printf("Cateto Adacente: %.2f%n", adj2);
 
 
+            if(oposto == 0.0F)
 
+
+                System.out.printf("Cateto Oposto: %.2f%n", ops2);
 
 
 
@@ -69,14 +75,7 @@ public class Trigonometria {
 
 
 
-
-
-
-
-
     }
-
-
 
 
 }
